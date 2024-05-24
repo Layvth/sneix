937d268ac79b16ce0a5fa495d807e9f685499b1d
dvsys <dvsys@localhost.localdomain>
2024-05-24 11:36:08 +0100

    add new function [ check_root ], [ switch to monitor mode ]

--- wifiBrute.sh
+++ wifiBrute.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # 
 #
-#
+# colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+
+
 #
 #
 os=$(uname)
@@ -15,26 +22,31 @@ function banner () {
  _ _ _|_|  _|_| __  |___ _ _| |_ ___ 
 | | | | |  _| | __ -|  _| | |  _| -_|
 |_____|_|_| |_|_____|_| |___|_| |___|                              
-Layvth@Github/
+Layvth@Github
 "
 }
 check_monitor_mode_support() {
     # Check if the phy80211 directory exists for the interface
     if [ -d "/sys/class/net/$interface/phy80211" ]; then
-        echo "[*]~: Interface ($interface) supports monitor mode !"
-        read -p "[*]~: Set ($interface) to monitor mode (yes/no) : " response
+        echo -e "${GREEN}[*]~: ${NC}Interface ($interface) supports monitor mode !"
+        sleep 2
+        read -p "      Switch ($interface) to monitor mode (yes/no) : " response
         if [ "$response" == "yes" ] || [ "$response" == "y" ]; then
             sleep .7
             airmon-ng start $interface >> /dev/null
-            echo "[*]~: $infterface switched to monitor mode !"
+            sleep .5
+            echo -e "${GREEN}[*]~:${NC}$infterface Switched to monitor mode !"
+            echo -e "${GREEN}[+]~:${NC} Tools checking :"
+            sleep 1
+            checkTools
         else
             echo "Exit "
             exit 1
         fi 
     else
-        echo "[!]~: Interface ($interface) does not support monitor mode"
+        echo -e "${RED}[!]~:${NC} Interface ($interface) does not support monitor mode"
         sleep .7
-        echo "[!]~: Error : check your interface !"
+        echo -e "${RED}[!]~:${NC} Error : check your interface !${NC}"
         
         exit 1
     fi
@@ -64,31 +76,27 @@ ctrl_c () {
 trap ctrl_c SIGINT
 
 # check for root permetion
-
-if [ "$(id -u)" != "0" ]; then
-    echo " we need root permition !"
-    echo " try run with [ sudo ./wifiBrute.sh] "
-fi
-# check for aircrack-ng
-
-air=$(which aircrack-ng)
-if ! [ "$?" -eq "0" ]; then
-    echo "aircrack-ng not found !"
-    echo "try [ apt-get install aircrack-ng ] "
-fi
-clear
-banner    
-check_monitor_mode_support
-
-
-
-
-
-
-
-
-
-
+checkRoot () { 
+    if [ "$(id -u)" != "0" ]; then
+        echo "[!] We need root permition !"
+        echo "[+] Try run with [ sudo ./wifiBrute.sh] "
+    else 
+        clear
+        banner
+        check_monitor_mode_support
+    fi
+} 
+checkTools () {
+    # check for aircrack-ng
+    air=$(which aircrack-ng)
+    if ! [ "$?" -eq "0" ]; then
+      echo -e "     Aircrack-ng     ${RED}[Not Found] ${NC}"
+      echo "[!] try [ apt-get install aircrack-ng ] " fi checkRoot
+    else 
+      echo -e "      Aircrack-ng            :${GREEN}[Found]${NC}"
+    fi
+}
+checkRoot
 
 
 
